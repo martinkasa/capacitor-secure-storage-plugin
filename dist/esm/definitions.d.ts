b3b73e8f490b1b2b9370127a1855300f05360a4f
Luca Chianese <lucad.chianese@gmail.com>
2020-09-24 23:53:12 +0200

    Include dist folder

---
+++ dist/esm/definitions.d.ts
@@ -0,0 +1,29 @@
+declare module '@capacitor/core' {
+    interface PluginRegistry {
+        SecureStoragePlugin: SecureStoragePluginPlugin;
+    }
+}
+export interface SecureStoragePluginPlugin {
+    get(options: {
+        key: string;
+    }): Promise<{
+        value: string;
+    }>;
+    set(options: {
+        key: string;
+        value: string;
+    }): Promise<{
+        value: boolean;
+    }>;
+    remove(options: {
+        key: string;
+    }): Promise<{
+        value: boolean;
+    }>;
+    clear(): Promise<{
+        value: boolean;
+    }>;
+    getPlatform(): Promise<{
+        value: string;
+    }>;
+}
