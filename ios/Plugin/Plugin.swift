5ce7752b2a72babb6020bff6578be40a0292be5b
Luca Chianese <lucad.chianese@gmail.com>
2020-11-12 19:31:07 +0100

    [iOS] Bugfix + function to get all the keys

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -8,49 +8,69 @@ import SwiftKeychainWrapper
  */
 @objc(SecureStoragePlugin)
 public class SecureStoragePlugin: CAPPlugin {
+    var keychainwrapper: KeychainWrapper = KeychainWrapper.init(serviceName: "cap_sec")
     
     @objc func set(_ call: CAPPluginCall) {
         let key = call.getString("key") ?? ""
         let value = call.getString("value") ?? ""
-        let saveSuccessful: Bool = KeychainWrapper.standard.set(value, forKey: key)
-        if(saveSuccessful) {
-            call.success([
-                "value": saveSuccessful
-            ])
-        }
-        else {
-            call.error("error")
-        }
+        let saveSuccessful: Bool = keychainwrapper.set(value, forKey: key)
+        call.success([
+            "value": saveSuccessful
+        ])
     }
     
     @objc func get(_ call: CAPPluginCall) {
         let key = call.getString("key") ?? ""
-        //let hasValue = KeychainWrapper.standard.hasValue(forKey: key)
+        let hasValueDedicated = keychainwrapper.hasValue(forKey: key)
+        let hasValueStandard = KeychainWrapper.standard.hasValue(forKey: key)
+        
+        // copy standard value to dedicated and remove standard key
+        if (hasValueStandard && !hasValueDedicated) {
+            let syncValueSuccessful: Bool = keychainwrapper.set(
+                KeychainWrapper.standard.string(forKey: key) ?? "",
+                forKey: key
+            )
+            let removeValueSuccessful: Bool = KeychainWrapper.standard.removeObject(forKey: key)
+            if (!syncValueSuccessful || !removeValueSuccessful) {
+                call.error("error")
+            }
+        }
+        
         call.success([
-            "value": KeychainWrapper.standard.string(forKey: key)
+            "value": keychainwrapper.string(forKey: key) ?? ""
         ])
-        /*
-        if(hasValue) {
-            call.success([
-                "value": KeychainWrapper.standard.string(forKey: key) ?? ""
-            ])
-        }
-        else {
-            call.error("Item with given key does not exist")
-        }
-        */
+    }
+    
+    @objc func keys(_ call: CAPPluginCall) {
+        let keys = keychainwrapper.allKeys();
+        call.success([
+            "value": keys
+        ])
     }
     
     @objc func remove(_ call: CAPPluginCall) {
         let key = call.getString("key") ?? ""
-        let removeSuccessful: Bool = KeychainWrapper.standard.removeObject(forKey: key)
+        KeychainWrapper.standard.removeObject(forKey: key);
+        let removeDedicatedSuccessful: Bool = keychainwrapper.removeObject(forKey: key)
         call.success([
-            "value": removeSuccessful
+            "value": removeDedicatedSuccessful
         ])
     }
     
     @objc func clear(_ call: CAPPluginCall) {
-        let clearSuccessful: Bool = KeychainWrapper.standard.removeAllKeys()
+        let keys = keychainwrapper.allKeys();
+        // cleanup standard keychain wrapper keys
+        for key in keys {
+            let hasValueStandard = KeychainWrapper.standard.hasValue(forKey: key)
+            if (hasValueStandard) {
+                let removeStandardSuccessful = KeychainWrapper.standard.removeObject(forKey: key)
+                if (!removeStandardSuccessful) {
+                    call.error("error")
+                }
+            }
+        }
+        
+        let clearSuccessful: Bool = keychainwrapper.removeAllKeys()
         call.success([
             "value": clearSuccessful
         ])
