e8f53672b766064d9c3da0c647a7b5066c09bb36
Laszlo Blum <laszlo.70.blum@gmail.com>
2023-11-07 13:08:09 +0100

    dist removed

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { SecureStoragePlugin as SecureStoragePluginType } from './definitions';
-declare const SecureStoragePlugin: SecureStoragePluginType;
-export * from './definitions';
-export { SecureStoragePlugin };
--- dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const SecureStoragePlugin = registerPlugin('SecureStoragePlugin', {
-    web: () => import('./web').then((m) => new m.SecureStoragePluginWeb()),
-});
-export * from './definitions';
-export { SecureStoragePlugin };
-//# sourceMappingURL=index.js.map
