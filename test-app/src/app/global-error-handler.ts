8ada7c0e3fadd46630005d6d9d71f2d34499528b
codepushr <1425202+codepushr@users.noreply.github.com>
2024-09-11 12:42:24 +0200

    fix: use stderr stream for global error handler

--- test-app/src/app/global-error-handler.ts
+++ test-app/src/app/global-error-handler.ts
@@ -3,6 +3,6 @@ import { ErrorHandler, Injectable } from "@angular/core";
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
   handleError(error: Error) {
-    console.log(error.toString());
+    console.error(error.toString());
   }
 }
