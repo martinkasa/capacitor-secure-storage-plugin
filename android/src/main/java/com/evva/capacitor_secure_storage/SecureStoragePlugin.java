4bac5827cf9191d8478193b37281af70c15ab409
0x7061 <1425202+0x7061@users.noreply.github.com>
2025-02-24 14:13:02 +0100

    fix: reintroduce `loadTextContext()` needed in instrumented tests

--- android/src/main/java/com/evva/capacitor_secure_storage/SecureStoragePlugin.java
+++ android/src/main/java/com/evva/capacitor_secure_storage/SecureStoragePlugin.java
@@ -1,5 +1,6 @@
 package com.evva.capacitor_secure_storage;
 
+import android.content.Context;
 import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
@@ -17,6 +18,10 @@ public class SecureStoragePlugin extends Plugin {
   public void load() {
     super.load();
     this.passwordStorageHelper = new PasswordStorageHelper(getContext());
+  }
+
+  public void loadTextContext(Context context) {
+    this.passwordStorageHelper = new PasswordStorageHelper(context);
   }
 
   @PluginMethod
